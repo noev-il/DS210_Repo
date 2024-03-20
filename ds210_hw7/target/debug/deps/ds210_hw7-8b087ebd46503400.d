8cc4e8c1450daeae78ddcc308e1dd1ebfba3b0bf
NoEvil <axusmaweasmelash@gmail.com>
2024-03-20 13:43:54 -0400

    creating new ds210_hw7 file

---
+++ ds210_hw7/target/debug/deps/ds210_hw7-8b087ebd46503400.d
@@ -0,0 +1,5 @@
+/Users/aux/Downloads/Test_Directory/ds210_hw7/target/debug/deps/ds210_hw7-8b087ebd46503400: src/main.rs
+
+/Users/aux/Downloads/Test_Directory/ds210_hw7/target/debug/deps/ds210_hw7-8b087ebd46503400.d: src/main.rs
+
+src/main.rs:
---
+++ ds210_hw7/target/debug/ds210_hw7.d
@@ -0,0 +1,1 @@
+/Users/aux/Downloads/Test_Directory/ds210_hw7/target/debug/ds210_hw7: /Users/aux/Downloads/Test_Directory/ds210_hw7/src/main.rs
